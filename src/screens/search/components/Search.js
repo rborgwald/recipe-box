719350783242707289ee87b89e15717bae3d13ab
Rachel Bufkes <rborgwald2885@gmail.com>
2017-09-09 09:19:02 -0500

    Updates layout of search screen

--- src/screens/search/components/Search.js
+++ src/screens/search/components/Search.js
@@ -13,49 +13,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    margin: 5,
   },
-  searchInputWrapper: {
+  infoWrapper: {
+    width: '100%',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 15,
-    marginBottom: 5,
+    justifyContent: 'space-between',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: '500',
-    alignContent: 'center',
+  detailsContainer: {
+    flex: 1,
+    flexDirection: 'column',
   },
   searchInput: {
     height: 40,
-    flex: 6,
     borderColor: '#666',
     borderWidth: 1,
-    margin: 5,
+    marginVertical: 15,
+    marginHorizontal: 5,
   },
   badgeContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: 8,
-  },
-  searchButtonContainer: {
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: '25%',
+    margin: 5,
+    marginBottom: 0,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 5,
+    marginBottom: 0,
   },
   searchButton: {
-    width: '100%',
     backgroundColor: '#29a709',
-  },
-  clearButtonContainer: {
-    width: '100%',
+    margin: 5,
+    width: '42%',
   },
   clearButton: {
-    fontSize: 16,
-    textAlign: 'right',
-    marginRight: 45,
+    backgroundColor: '#e24949',
+    margin: 5,
+    width: '42%',
+  },
+  buttonText: {
+    fontSize: 20,
   },
   searchResultsWrapper: {
     margin: 10,
@@ -120,66 +120,69 @@ const Search = ({
   errorMessage: string,
 }) =>
   <View style={styles.container}>
-    <View style={styles.searchInputWrapper}>
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Enter recipe to search"
-        value={textValue}
-        autoCorrect={false}
-        autoCapitalize="none"
-        underlineColorAndroid="rgba(0,0,0,0)"
-        onChangeText={onTextChange}
-        placeholderTextColor="#666"
-        onSubmitEditing={onSearchRecipe}
-      />
-      <View style={styles.searchButtonContainer}>
-        <BlockButton
-          style={styles.searchButton}
-          text="Search"
-          onPress={onSearchRecipe}
+    <View style={styles.infoWrapper}>
+      <View style={styles.detailsContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Enter recipe to search"
+          value={textValue}
+          autoCorrect={false}
+          autoCapitalize="none"
+          underlineColorAndroid="rgba(0,0,0,0)"
+          onChangeText={onTextChange}
+          placeholderTextColor="#666"
+          onSubmitEditing={onSearchRecipe}
+        />
+        <View style={styles.buttonContainer}>
+          <BlockButton
+            style={styles.searchButton}
+            textStyle={styles.buttonText}
+            text="Search"
+            onPress={onSearchRecipe}
+          />
+          <BlockButton
+            style={styles.clearButton}
+            textStyle={styles.buttonText}
+            text="Clear"
+            onPress={onClearSearch}
+          />
+        </View>
+      </View>
+
+      <View style={styles.badgeContainer}>
+        <BadgeSelector
+          childRef={getRefFromTypes(types, 'mealTypes')}
+          searchCriterion={getValueFromTypes(types, 'mealTypes')}
+          backgroundColor="#6b7a8f"
+          defaultText="Any meal"
+          onValueChange={getCallbackFromTypes(types, 'mealTypes')}
+          options={getOptionsFromTypes(types, 'mealTypes')}
+        />
+        <BadgeSelector
+          childRef={getRefFromTypes(types, 'cuisineTypes')}
+          searchCriterion={getValueFromTypes(types, 'cuisineTypes')}
+          backgroundColor="#f7882f"
+          defaultText="Any cuisine"
+          onValueChange={getCallbackFromTypes(types, 'cuisineTypes')}
+          options={getOptionsFromTypes(types, 'cuisineTypes')}
+        />
+        <BadgeSelector
+          childRef={getRefFromTypes(types, 'preparationTypes')}
+          searchCriterion={getValueFromTypes(types, 'preparationTypes')}
+          backgroundColor="#f7c331"
+          defaultText="Any prep"
+          onValueChange={getCallbackFromTypes(types, 'preparationTypes')}
+          options={getOptionsFromTypes(types, 'preparationTypes')}
+        />
+        <BadgeSelector
+          childRef={getRefFromTypes(types, 'proteinTypes')}
+          searchCriterion={getValueFromTypes(types, 'proteinTypes')}
+          backgroundColor="#dcc7aa"
+          defaultText="Any protein"
+          onValueChange={getCallbackFromTypes(types, 'proteinTypes')}
+          options={getOptionsFromTypes(types, 'proteinTypes')}
         />
       </View>
-    </View>
-    <View style={styles.clearButtonContainer}>
-      <WordButton
-        style={styles.clearButton}
-        text="Clear"
-        onPress={onClearSearch}
-      />
-    </View>
-    <View style={styles.badgeContainer}>
-      <BadgeSelector
-        childRef={getRefFromTypes(types, 'mealTypes')}
-        searchCriterion={getValueFromTypes(types, 'mealTypes')}
-        backgroundColor="#6b7a8f"
-        defaultText="Any meal"
-        onValueChange={getCallbackFromTypes(types, 'mealTypes')}
-        options={getOptionsFromTypes(types, 'mealTypes')}
-      />
-      <BadgeSelector
-        childRef={getRefFromTypes(types, 'cuisineTypes')}
-        searchCriterion={getValueFromTypes(types, 'cuisineTypes')}
-        backgroundColor="#f7882f"
-        defaultText="Any cuisine"
-        onValueChange={getCallbackFromTypes(types, 'cuisineTypes')}
-        options={getOptionsFromTypes(types, 'cuisineTypes')}
-      />
-      <BadgeSelector
-        childRef={getRefFromTypes(types, 'preparationTypes')}
-        searchCriterion={getValueFromTypes(types, 'preparationTypes')}
-        backgroundColor="#f7c331"
-        defaultText="Any prep"
-        onValueChange={getCallbackFromTypes(types, 'preparationTypes')}
-        options={getOptionsFromTypes(types, 'preparationTypes')}
-      />
-      <BadgeSelector
-        childRef={getRefFromTypes(types, 'proteinTypes')}
-        searchCriterion={getValueFromTypes(types, 'proteinTypes')}
-        backgroundColor="#dcc7aa"
-        defaultText="Any protein"
-        onValueChange={getCallbackFromTypes(types, 'proteinTypes')}
-        options={getOptionsFromTypes(types, 'proteinTypes')}
-      />
     </View>
     <Text style={styles.errorMessage}>
       {errorMessage}
